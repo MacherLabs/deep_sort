e51d1a9611986a57441e5ac85cfb2401970ea744
aestaq <atinderpalap@gmail.com>
2018-03-22 19:53:51 +0530

    Added working deep sort tracking algo in the form of pip package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup( 
+    name='deep_sort',
+    
+    version='0.10',
+    description='Simple Online and Realtime Tracking with a Deep Association Metric',
+    url='http://demo.vedalabs.in/',
+
+    # Author details    
+    author='Atinderpal Singh',
+    author_email='atinderpalap@gmail.com',
+    
+    license='Commercial',
+
+    packages=['deep_sort', 'deep_sort.deep_sort', 'deep_sort.application_util'],
+
+    install_requires=['numpy', 'sklearn'],
+    zip_safe=False
+    )
